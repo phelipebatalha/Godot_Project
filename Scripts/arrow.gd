5f67483c5a01c3df1d70a72f04c8907b0ad357b1
Phelipe Henrique Batalha <phelipebatalha@hotmail.com>
2024-11-28 15:54:27 -0300

    Criei o inimigo Slime
    Foi-se criado o inimigo Slime. Ele possui: hitbox para detecção de dano recebico. No mesmo o objeto que causou dano, sendo a flecha, é destruído. Além disso, ele possui área de detecção para encontrar o player e persegui-lo.
    
    No quesito artístico, possui animação de: Movimentação, Morte e Idle.
    
    Importante: O inimigo não causa dano

--- Scripts/arrow.gd
+++ Scripts/arrow.gd
@@ -11,7 +11,8 @@ func _process(delta):
 	position += (Vector2.RIGHT*speed).rotated(rotation) * delta
 	
 	
-
+func arrow_deal_damage():
+	pass
 
 func _on_visible_on_screen_enabler_2d_screen_exited() -> void:
 	queue_free()
